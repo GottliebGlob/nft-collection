4826139b805a8020773b705b145a8bae5d58f185
GottliebGlob <globingleb13@gmail.com>
2022-05-01 00:53:19 +0300

    faq fix

--- src/components/Faq/FAQ.tsx
+++ src/components/Faq/FAQ.tsx
@@ -22,34 +22,29 @@ const theme = useTheme()
                 <ShimmerTitle style={{
 
                 }}>
-                    <img src={question} alt="loading..." style={{height: 30, marginRight: 3}}/>
                     FAQ
-                    <img src={question} alt="loading..." style={{height: 30, marginLeft: 3}}/>
                 </ShimmerTitle>
-            <FaqItem question="Why should I buy Darkland Creatures Cards NFTs"
+            <FaqItem question="Why should I buy Darkland Creatures Cards NFTs❓"
                      answer="Holding Darkland Creatures Cards NFTs will
                      allow you to get a whitelist on a Polygon p2e game"/>
 
-                <FaqItem question="MINT price?"
+                <FaqItem question="MINT price❓"
                          answer="0.1 SOL for public mint and 0.08 for whitelist"/>
 
-                <FaqItem question="What about NFT game"
+                <FaqItem question="What about NFT game❓"
                          answer="This is a p2e game where you can earn ONYX tokens.
                          Each character receive a certain amount of tokens for 24 hours.
                          You can improve its performance by buying clothes and equipment.
                          Tokens can be exchanged on exchanges."/>
 
-                <FaqItem question="Secondary markets"
+                <FaqItem question="Secondary markets❓"
                          answer="We have already received approval on Magical Eden."/>
 
-                <FaqItem question="What about ONYX token"
+                <FaqItem question="What about ONYX token❓"
                          answer="ONYX is an in-game currency earned by your NFTs in Forest Industries.
                          It can be used to improve the characteristics of the characters or exchanged."/>
 
-                <FaqItem question="Is our game on Solana"
-                         answer="No. It will be build on Polygon blockchain."/>
-
-                <FaqItem question="How to get whitelist"
+                <FaqItem question="How to get whitelist❓"
                          answer="Be the holder of 1 or more NFT Darkland Creatures collection or
                          Invite 3 friends to our discord and and Reach Level 6"/>
         </DesContainer>
